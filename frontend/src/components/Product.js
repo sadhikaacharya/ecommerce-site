87d8605c36f728fcd1302a00b97d007a62783c75
Sadhika Acharya <acharyasadhika@gmail.com>
2022-07-22 22:04:52 +0545

    adding bootstrap components

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Rating from './Rating'
+
+function Product(props){
+    const {product} = props
+    return(
+    <Card >
+    <Link to={`/product/${product.slug}`}>
+       <img width='100%' max-height='75%' src={product.image} className="card-img-top" alt={product.name}/>
+    </Link>
+    <Card.Body>
+    <Link to={`/product/${product.slug}`}>
+      <Card.Title> {product.name}</Card.Title>
+    </Link>
+    <Rating rating={product.rating} numReviews={product.numReviews} />
+    <Card.Text> ${product.price}</Card.Text>
+    <Button> Add to Cart</Button>
+    </Card.Body>
+  </Card>)
+}
+export default Product
