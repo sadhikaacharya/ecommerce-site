de4b330119b58ceba27707507a450afb5cc678be
Sadhika Acharya <acharyasadhika@gmail.com>
2022-07-22 20:48:59 +0545

    Connecting frontend and backend

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Rib Knit Cardigan',
+            slug: 'rib-knit-cardigan',
+            category:'Tops',
+            image: '/images/p1.jpeg',
+            price: 20,
+            countInStock: 10,
+            brand: 'Princess Polly',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'trendy cardigan top'
+        },
+        {
+            name: 'Bodycon dress',
+            slug: 'bodycon-dress',
+            category:'Dress',
+            image: '/images/p2.webp',
+            price: 45,
+            countInStock: 5,
+            brand: 'Wild Fable',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'party dresses'
+        },
+        {
+            name: 'Floral Dress',
+            slug: 'floral-dress',
+            category:'Dress',
+            image: '/images/p3.jpeg',
+            price: 25,
+            countInStock: 13,
+            brand: 'H&M',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'cute summer dress'
+        },
+        {
+            name: 'Plain Crop Top',
+            slug: 'plain-crop-top',
+            category:'Tops',
+            image: '/images/p4.jpeg',
+            price: 15,
+            countInStock: 9,
+            brand: 'H&M',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'basic staple for your wardrobe'
+        }
+    ]
+}
+
+export default data
